b8a2a0598576d1db2f2c1c4f61a883f704032908
Michael Pope <popemray1986@gmail.com>
2019-01-20 04:53:35 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+
 class RestaurantInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = e => {
+      this.setState({
+        text: e.target.value
+      });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({text: ''});
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={e => this.handleChange(e)}
+          />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
